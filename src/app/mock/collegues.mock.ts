64758ed198568ec6acc4204fe1fd90756635edea
JeremYD <delpech.jeremy@hotmail.fr>
2020-07-30 12:21:14 +0200

    Angular: TP Composants

--- src/app/mock/collegues.mock.ts
+++ src/app/mock/collegues.mock.ts
@@ -1,4 +1,4 @@
 import { Collegue } from '../models/Collegue';
 
 // Création et export d'un collègue
-export const collegueMock = new Collegue('AB18', 'Duch', 'Martin', 'mduch@mail.org', new Date(1992, 5, 14), '../assets/photo_duch.png');
+export const collegueMock = new Collegue('AB18', 'Duch', 'Martin', 'mduch@mail.org', new Date(1992, 5, 14), 'assets/photo_duch.png');

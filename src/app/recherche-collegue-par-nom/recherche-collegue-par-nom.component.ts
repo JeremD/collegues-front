698dba0426b1d92bad5c664517520ef8eb778f8c
JeremYD <delpech.jeremy@hotmail.fr>
2020-07-31 12:41:04 +0200

    Angular: RechercherParNom (avec correction)

--- src/app/recherche-collegue-par-nom/recherche-collegue-par-nom.component.ts
+++ src/app/recherche-collegue-par-nom/recherche-collegue-par-nom.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { matriculesMock } from '../mock/matricules.mock';
 
 @Component({
   selector: 'app-recherche-collegue-par-nom',
@@ -7,14 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RechercheCollegueParNomComponent implements OnInit {
 
+  // Récupération de la liste des matricules
+  matriculesMock = matriculesMock;
+  recherche = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  // Rechercher un client
-/*rechercher(): string {
-
-}*/
+  // Afficher la liste des matricules
+  rechercher(): void {
+    this.recherche = true;
+  }
 
 }
